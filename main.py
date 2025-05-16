bda6e016b8f8260396c1644af1fb0fec7f16e73e
LASooli <rawirinathan@gmail.com>
2025-05-16 17:16:33 +1000

    P0=P9=P19, thus all last term of thrust goes to 0.
    - added M9 function
    - changed TC ratio
    - Changed formulas from _() to (). (looks better, will deal with consequences later - is fine in this case)
    - Added tau_t function
    
    Next:
    - Need some tau values. Or change SFC formula to not have tau.
     - Need pi_0, from table? can make a function for pi_0 from M0.
     - Function for mass flow rate m0 from M0?
     - Create function for selecting operating mode and setting relevant values to 0 or 1 (switch:case)?
     - Function that computes engine performance as function of M0 and operating mode

--- main.py
+++ main.py
@@ -16,60 +16,63 @@ alpha = 1; mC = m0/(1+alpha); mB = m0*alpha/(1+alpha)
 
 #tau_0 = x; tau_b = x; tau_lambda = x; tau_lambAB = x; tau_B = x; tau_AB = x; tau_f = x;
 
-#tau_c = pi_C*(gamma-1)/(gamma*e_c); tau_t = pi_T*(e_t*(gamma-1)/gamma);  #which gamma?
+#tau_c = pi_C*(gamma-1)/(gamma*e_c); 
 
 #M0 = varies;
 
-def TB_ratio_():
+def TB_ratio():
     """ Bypass Temperature Ratio T19/T0 """ 
     TB_ratio = tau_fn*tau_B*tau_f*tau_0
     return TB_ratio
 
-def TC_ratio_():
+def TC_ratio():
     """ Core Temperature Ratio T9/T0 """
-    TC_ratio = tau_lambda*tau_t*tau_AB*(1/c_ratio)
+    TC_ratio = tau_lambda*tau_t*(1/c_ratio) / ((1*pi0*pi_dmax*pi_C*pi_b*pi_t*pi_n)**((gamma_t-1)/gamma_t))
     return TC_ratio
 
-def PB_ratio_():
+def PB_ratio():
     """ Pressure stagnation ratio across Bypass Pt19/P19 """
     return 
 
-def PC_ratio_():
+def PC_ratio():
     """ Pressure stagnation ratio across Core Pt9/P9 """ 
     return 
 
-def M9_():
-    """ Need to define Pt19/P0 """
-    return (2/(gamma_t-1))*(())
+def M9():
+    """ Mach number at point 9 """
+    return (2/(gamma_t-1))*((1*pi0*pi_dmax*pi_C*pi_b*pi_t*pi_n)**((gamma_t-1)/gamma_t)-1)
 
-def M19_():
-    """ Need to define Pt9/P0 """
+def M19():
+    """ Mach number at point 19 """
     return 
 
-def f_():
+def f():
     """ Calculates fuel air ratio for burner """
     f = (((eff_b*H)/(c_pc*T_0))**-1)*(tau_lambda - tau_c*tau_0)
     return f
 
-def f_B_():
+def f_B():
     """ Caclulates fuel air ratio for bypass """
     f_B = ((((eff_B*H)/(c_pc*T_0)) - tau_B*tau_f*tau_0)**-1)*(tau_B*tau_f*tau_0 - tau_f*tau_0)
     return f_B
 
-def f_AB_():
+def f_AB():
     """ Calculates fuel air ratio for after burner """
-    f_AB = ((((eff_AB*H)/(c_pt*T_0)) - tau_AB*tau_t*tau_lambda)**-1)*(1+f_())*(tau_AB*tau_t*tau_lambda - tau_t*tau_lambda)
+    f_AB = ((((eff_AB*H)/(c_pt*T_0)) - tau_AB*tau_t*tau_lambda)**-1)*(1+f())*(tau_AB*tau_t*tau_lambda - tau_t*tau_lambda)
     return f_AB
 
-def F_():
+def tau_t():
+    return 1-1/(eff_m*(1+f()))*tau_0/tau_lambda*(tau_c-1);
+
+def pi_t():
+    return tau_t() ** (gamma_t/((gamma_t-1)*e_t))
+
+def F():
     """ Calculates overall thrust """
-    Fcore = m0*(a0/(1+alpha)*((1+f_())*(   np.sqrt(gamma_t*Rt*TC_ratio_()*gamma_c*Rc) * M9 + np.sqrt(gamma_c*Rt*TC_ratio_()*gamma_t*Rc) * (1-PC_ratio_())/(M9*gamma_c)  )-M0))
-    Fbypass = m0*(alpha*a0/(1+alpha)*((1+f_B_())*(   np.sqrt(gamma_t*Rt*TB_ratio_()*gamma_c*Rc) * M19 + np.sqrt(gamma_c*Rt*TB_ratio_()*gamma_t*Rc) * (1-P0/P19)/(M19*gamma_c)  )-M0))
+    Fcore = m0*(a0/(1+alpha))*((1+f()+f_AB())*(   np.sqrt(gamma_t*Rt*TC_ratio()*gamma_c*Rc) * M9)-M0)
+    Fbypass = m0*(alpha*a0/(1+alpha))*((1+f_B())*(   np.sqrt(gamma_t*Rt*TB_ratio()*gamma_c*Rc) * M19)-M0) # P0=P9, so last term all goes to 0.
     return Fcore+Fbypass
 
-def SFC_():
+def SFC():
     """ Calculates specifc fuel consumption """
-    return ((m0/(1 + alpha)) * (f_() + f_B_()*alpha + f_AB_())) / F_()
-
-
-
+    return ((m0/(1 + alpha)) * (f() + f_B()*alpha + f_AB())) / F()
