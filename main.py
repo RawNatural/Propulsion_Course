ebe6d5f3855b027337841e73ee05d1fb4406903a
LASooli <rawirinathan@gmail.com>
2025-05-19 19:10:00 +1000

    It runs

--- main.py
+++ main.py
@@ -38,8 +38,9 @@ tau_n = 1 # Nozzle Stagnation Temperature Ratio
 T_t4max = 2000 # [K] - Turbine Entry Stagnation Temperature
 tau_lambda = (c_pt/c_pc)*(T_t4max/T_0)
 #f = mf/mc # Burner Fuel Air Ratio
-tau_AB = None #T_t7/T_t5
 #f_AB = mf_AB/mc # Afterburner Fuel Air Ratio
+tau_AB = 1.5 #T_t7/T_t5 #Also must initialise as something - design parameter. Can we figure this out based on combustion or something?
+
 
 """ Bypass Modelling Parameters """
 pi_f = 2.0 # Fan Stagnation Pressure Ratio
@@ -47,9 +48,10 @@ pi_fn = 0.95 # Nozzle Stagnation Pressure Ratio
 tau_fn = 1 # Nozzle Stagnation Temperature Ratio
 e_f = 2.0 # Fan Polytropic Efficiency
 #f_B = mf_B/mB # Bypass Fuel Air Ratio
-tau_B = 1 #T_t14/T_t13 # must initialise as 1 because it uses itself when finding itself.
 #pi_B = P_t14/P_t13
 eff_B = eff_AB # Otherwise tau_B = 1
+
+tau_B = 1.5 #T_t14/T_t13 # must initialise because it uses itself when finding itself.
 
 R = c_pc*(1-1/gamma_c)
 a0 = np.sqrt(gamma_c*R*T_0)
@@ -59,8 +61,8 @@ alpha = 1
 mC = None; mB = None;
 
 
-tau_c = (pi_c)**((gamma_c - 1)/gamma_c)
-tau_f = (pi_f)**((gamma_c - 1)/gamma_c)
+tau_c = (pi_c)**e_t*((gamma_c - 1)/gamma_c)
+tau_f = (pi_f)**e_t*((gamma_c - 1)/gamma_c)
 tau_fn = 1
 
 def tau_0(M0):
@@ -82,7 +84,7 @@ tau = {
     "0": None, #functions of M0
     "t": None,
     "AB": None,
-    "B": tau_B,
+    "B": None,
 }
 
 pi = {
@@ -143,32 +145,38 @@ def f_():
     denominator = (eff_b*H)/(c_pc*T_0)
     return numerator/denominator
 
-def f_B_():
+def f_B_(tB):
     """ Caclulates fuel air ratio for bypass """
-    numerator = tau['B']*tau['f']*tau['0'] - tau['f']*tau['0']
-    denominator = ((eff_B*H)/(c_pc*T_0)) - tau['B']*tau['f']*tau['0']
+    numerator = tB*tau['f']*tau['0'] - tau['f']*tau['0']
+    denominator = ((eff_B*H)/(c_pc*T_0)) - tB*tau['f']*tau['0']
     return numerator/denominator
 
 def tau_B(M0):
     """ Calculates tau['B'] """
-    numerator = f_B_()*(eff_B*H/(c_pc*T_0)) + tau['f']*tau_0(M0)
-    denominator = tau['f']*tau_0(M0) + f_B_()*tau['f']*tau_0(M0)
-    return numerator/denominator
+    tauB = 0; tB = 1;
+    while abs(tB - tauB) < 0.1:
+        numerator = f_B_(tB)*(eff_B*H/(c_pc*T_0)) + tau['f']*tau_0(M0)
+        denominator = tau['f']*tau_0(M0) + f_B_()*tau['f']*tau_0(M0)
+        tauB = numerator/denominator
+    return tauB
 
 def pi_B(M0):
-    """ Assuming Isentropic across bypass burner """
+    """ Assuming Isentropic across bypass burner """ #Task sheet says to use Rayleigh flow as in a ramjet (lecture 8 maybe)
     return tau_B(M0)**(gamma_t/(gamma_t -1))
 
-def f_AB_():
+def f_AB_(tauAB):
     """ Calculates fuel air ratio for after burner """
-    numerator = (1+f_())*(tau['AB']*tau['t']*tau['lambda'] - tau['t']*tau['lambda']) 
-    denominator = ((eff_AB*H)/(c_pt*T_0)) - tau['AB']*tau['t']*tau['lambda']
+    numerator = (1+f_())*(tauAB*tau['t']*tau['lambda'] - tau['t']*tau['lambda']) 
+    denominator = ((eff_AB*H)/(c_pt*T_0)) - tauAB*tau['t']*tau['lambda']
     return numerator/denominator
 
-def tau_AB():
-    numerator = f_AB_() * (eff_AB*H/(c_pt*T_0))+tau['t']*tau['lambda']*(1 + f_())
-    denominator = tau['t']*tau['lambda']*(1 + f_()) + f_AB_()*tau['t']*tau['lambda']
-    return numerator/denominator
+def tau_AB_():
+    tauAB = 0; tAB = 1;
+    while abs(tAB - tauAB) < 0.1:
+        numerator = f_AB_(tauAB) * (eff_AB*H/(c_pt*T_0))+tau['t']*tau['lambda']*(1 + f_())
+        denominator = tau['t']*tau['lambda']*(1 + f_()) + f_AB_(tauAB)*tau['t']*tau['lambda']
+        tauAB = numerator/denominator
+    return tauAB
 
 def pi_t(M0):
     return tau_t(M0) ** (gamma_t/((gamma_t-1)*e_t))
@@ -194,7 +202,8 @@ def setMode(mode, M0):
     if mode == 1:
         tau.update({"c": tau_c,
         "f": tau_f,
-        "t": None,
+        "t": tau_t(M0),
+        "0": tau_0(M0),
         "AB": 1,
         "B": 1,});
         pi.update({"c": pi_c,
@@ -210,8 +219,9 @@ def setMode(mode, M0):
         tau.update({
         "c": tau_c,
         "f": tau_f,
-        "t": None,
-        "AB": None,
+        "t": tau_t(M0),
+        "0": tau_0(M0),
+        "AB": tau_AB,
         "B": 1})
         pi.update({"c": pi_c,
         "AB": pi_AB,
@@ -219,15 +229,16 @@ def setMode(mode, M0):
         "t": None,
         "B": 1,
         "0": None } )
-        f_AB = f_AB_()
+        f_AB = f_AB_(tau['AB'])
         f_B = 0
 
     elif mode == 3:
         tau.update({"c": tau_c,
         "f": tau_f,
-        "t": None,
-        "AB": None,
-        "B": None})
+        "t": tau_t(M0),
+        "0": tau_0(M0),
+        "AB": tau_AB,
+        "B": tau_B})
         pi.update({"c": pi_c,
         "AB": pi_AB,
         "f": pi_f,
@@ -235,15 +246,17 @@ def setMode(mode, M0):
         "B": None,
         "0": None })
         f = f_()
-        f_B = f_B_()
-        f_AB = f_AB_()
+        f_B = f_B_(tau['B'])
+        f_AB = f_AB_(tau['AB'])
 
     elif mode == 4:
+        # - Also in ramjet mode, there is shock losses, so Pt2/Pt0 is something else. See lecture 8.
         tau.update({"c": 1,
         "f": 1,
         "t": 1,
-        "AB": None,
-        "B": None})
+        "0": tau_0(M0),
+        "AB": tau_AB,
+        "B": tau_B(M0)})
         pi.update({"c": 1,
         "AB": pi_AB,
         "f": 1,
@@ -251,8 +264,8 @@ def setMode(mode, M0):
         "B": None,
         "0": None })
         f = f_()
-        f_B = f_B_()
-        f_AB = f_AB_()
+        f_B = f_B_(tau['B'])
+        f_AB = f_AB_(tau['B'])
 
 """ Main Code """
 
@@ -261,7 +274,7 @@ M0 = np.linspace(0.1, 10, 10)
 tau['0'] = tau_0(M0)
 tau['t'] = tau_t(M0)
 tau['B'] = tau_B(M0)
-tau['AB'] = tau_AB()
+tau['AB'] = tau_AB_()
 pi["t"] = pi_t(M0)
 pi["B"] = pi_B(M0)
 pi["0"] = pi_0(M0)
